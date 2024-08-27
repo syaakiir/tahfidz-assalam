56f9816898ab385d5fbf58008ce2c06433584c4a
syahrul <syahrulayakir14@gmail.com>
2024-08-27 21:51:17 +0700

    pwa

--- resources/views/login/indexlogin.blade.php
+++ resources/views/login/indexlogin.blade.php
@@ -22,6 +22,11 @@
     <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="<?= URL::to('/') ?>/layout_login/css/util.css">
     <link rel="stylesheet" type="text/css" href="<?= URL::to('/') ?>/layout_login/css/main.css">
+
+    <!-- PWA  -->
+    <meta name="theme-color" content="#6777ef" />
+    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
     <!--===============================================================================================-->
 </head>
 
@@ -55,6 +60,23 @@
     <!--===============================================================================================-->
     <script src="<?= URL::to('/') ?>/layout_login/js/main.js"></script>
 
+    <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+        if ("serviceWorker" in navigator) {
+            // Register a service worker hosted at the root of the
+            // site using the default scope.
+            navigator.serviceWorker.register("/sw.js").then(
+                (registration) => {
+                    console.log("Service worker registration succeeded:", registration);
+                },
+                (error) => {
+                    console.error(`Service worker registration failed: ${error}`);
+                },
+            );
+        } else {
+            console.error("Service workers are not supported.");
+        }
+    </script>
 </body>
 
 </html>
